625626fa1eda0304eb8582b9d03751df2b5ed336
Brad <brad.cocar@gmail.com>
2021-12-15 14:41:22 -0800

    Cards now randomize after each click

--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -4,6 +4,7 @@ import MemoryCard from './MemoryCard';
 const CardDisplay = (props) => {
     const cards = [0, 1, 2, 3, 4, 5];
     const [selectedCards, setSelectedCards] = useState([]);
+    const components = [];
     const handleClick = (id) => {
         for(let i = 0; i <  selectedCards.length; i++) {
             if(selectedCards[i] === id) {
@@ -12,11 +13,25 @@ const CardDisplay = (props) => {
         }
         setSelectedCards(selectedCards => [...selectedCards, id]);
     }
+    const generateCard = () => {
+        const usedNums = [];
+        for(let i = 0; i < cards.length; i++) {
+            let randomNum;
+            do {
+            randomNum = Math.floor(Math.random() * (cards.length));
+            } while(usedNums.some(element => element === randomNum))
+            components.push(<MemoryCard id={cards[randomNum]} handleClick={handleClick}/>);
+            usedNums.push(randomNum);
+        }
+        return(
+            <div className='card-holder'>
+                {components.map((comp, i) => <div key={i}>{comp}</div>)}
+            </div>
+        );
+    }
     return (
-        <div className='card-holder'>
-            <MemoryCard id={cards[0]} handleClick={handleClick}/>
-            <MemoryCard id={cards[1]} handleClick={handleClick}/>
-            <MemoryCard id={cards[2]} handleClick={handleClick}/>
+        <div className>
+            {generateCard()}
         </div>
     )
 }
